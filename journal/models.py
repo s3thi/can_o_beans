a60ac7dde3d32091c07a698f230f0976b779cdf9
Ankur Sethi <contact@ankursethi.in>
2012-03-15 19:54:41 +0530

    Created a JournalEntry model.

--- journal/models.py
+++ journal/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class JournalEntry(models.Model):
+	title = models.CharField(max_length=512, blank=True, null=True)
+	published_on = models.DateTimeField()
+	content = models.TextField()
+	author = models.ForeignKey(User)
+
+	def __unicode__(self):
+		if self.title:
+			return '<{0}>'.format(self.title)
+		else:
+			humanized_date = self.published_on.strftime('%b %d, %Y')
+			return '<{0}: {1}>'.format(humanized_date, self.content[:128])
