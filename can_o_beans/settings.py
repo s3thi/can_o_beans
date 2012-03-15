1399aa7fdd8a5a8475939ab3aa2cac5401936727
Ankur Sethi <contact@ankursethi.in>
2012-03-15 18:54:39 +0530

    Move to Django 1.4 release candidate.

--- can_o_beans/settings.py
+++ can_o_beans/settings.py
@@ -102,6 +102,9 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'can_o_beans.urls'
 
+# Python dotted path to the WSGI application used by Django's runserver.
+WSGI_APPLICATION = 'can_o_beans.wsgi.application'
+
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
