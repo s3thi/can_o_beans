c871cb061cc8c8bb40b2f2410a7424d24490afab
Ankur Sethi <contact@ankursethi.in>
2012-03-15 18:54:03 +0530

    Fixed small URL glitches and created stub views.

--- journal/urls.py
+++ journal/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls.defaults import patterns, include, url
 
 y = '(?P<year>\d{4})'
 m = '(?P<month>\w+)'
-d = '(?P<day>\d{2})'
-t = '(?P<title>\w+)'
+d = '(?P<day>\d{1,2})'
+t = '(?P<title>\S+)'
 
 urlpatterns = patterns('journal.views',
+	url('^$', 'show_index'),
 	url('^{0}/$'.format(y), 'show_archive'),
 	url('^{0}/{1}/$'.format(y, m), 'show_archive'),
 	url('^{0}/{1}/{2}/$'.format(y, m, d), 'show_archive'),
-	url('^{0}/{1}/{2}/{3}/$'.format(y, m, d, t), 'show_post'),
+	url('^{0}/{1}/{2}/{3}/$'.format(y, m, d, t), 'show_entry'),
 )
--- journal/views.py
+++ journal/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def show_index(request):
+	return show_archive(request)
+
+def show_archive(request, year=None, month=None, day=None):
+	return HttpResponse('showing archive')
+
+def show_entry(request, year, month, day, title):
+	return HttpResponse('showing a post')
