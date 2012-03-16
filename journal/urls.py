00e239552503352092d5ffac4d76d7fa772a8552
Ankur Sethi <contact@ankursethi.in>
2012-03-16 14:00:59 +0530

    Change "title" to "slug" and use the new generic JournalEntryListView for viewing archives.

--- journal/urls.py
+++ journal/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
+from journal.views import JournalEntryListView
 
 y = '(?P<year>\d{4})'
 m = '(?P<month>\w+)'
 d = '(?P<day>\d{1,2})'
-t = '(?P<title>\S+)'
+s = '(?P<slug>\S+)'
 
 urlpatterns = patterns('journal.views',
-    url('^$', 'show_index'),
-    url('^{0}/$'.format(y), 'show_archive'),
-    url('^{0}/{1}/$'.format(y, m), 'show_archive'),
-    url('^{0}/{1}/{2}/$'.format(y, m, d), 'show_archive'),
-    url('^{0}/{1}/{2}/{3}/$'.format(y, m, d, t), 'show_entry'),
+    url('^$', JournalEntryListView.as_view()),
+    url('^{0}/$'.format(y), JournalEntryListView.as_view()),
+    url('^{0}/{1}/$'.format(y, m), JournalEntryListView.as_view()),
+    url('^{0}/{1}/{2}/$'.format(y, m, d), JournalEntryListView.as_view()),
+    url('^{0}/{1}/{2}/{3}/$'.format(y, m, d, s), 'show_entry'),
 )
