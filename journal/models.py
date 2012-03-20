1bba7f0ea88c2c7c8e2ecd6efb72ee7a22c936c5
Ankur Sethi <contact@ankursethi.in>
2012-03-21 00:31:31 +0530

    Don't use the post body to generate a slug. Also, code cleanup.

--- journal/models.py
+++ journal/models.py
@@ -15,22 +15,18 @@ Can 'o Beans will automatically try to create one using the title of the entry.
 For example, an entry titled "I Don't Like These New Bamboo Mats" will have
 "i-dont-like-these-new-bamboo-mats" as its slug.
 
-If both the slug and the title for an entry are missing, Can 'o Beans will
-use the first 256 characters of the entry to generate a slug.
-
 If this a slug is not unique, Can 'o Beans will eat a few characters off
 the end of the slug to make room for a unique numeric identifier. First, a  "-1"
 will be appended to the end of the slug. If the new slug is still not unique, the
 "-1" will be incremented to "-2", and so on until a unique slug has been found.
 '''
 
+SLUG_MAXLEN = 256
 
 class JournalEntry(models.Model):
 
-    SLUG_MAXLEN = 256
-
     title = models.TextField(blank=True, null=True)
-    slug = models.SlugField(max_length=SLUG_MAXLEN, blank=True)
+    slug = models.SlugField(max_length=SLUG_MAXLEN, blank=True, null=True)
     published_on = models.DateTimeField()
     content = models.TextField(blank=True, null=True)
     author = models.ForeignKey(User)
@@ -39,28 +35,27 @@ class JournalEntry(models.Model):
         if self.title:
             return self.title
         else:
-            return self.slug
+            return self.content[:128]
 
     def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = self.make_unique_slug()
+        if self.title and not self.slug:
+            self.slug = self.unique_slug_from_title()
         
         return super(JournalEntry, self).save(*args, **kwargs)
 
-    def make_unique_slug(self):
-        # First, generate a slug.
-        # TODO: raise error if body, title and slug are all null.
-        if self.title:
-            slug = slugify(self.title)
-        else:
-            slug = slugify(self.content[:SLUG_MAXLEN])
+    def unique_slug_from_title(self):
+        slug = slugify(self.title)
 
-        # Now ensure that the generated slug is unique.
-        entries = JournalEntry.objects.filter(published_on=self.published_on)
-        slugs = [ entry.slug for entry in entries ]
+        def _check_duplicates(s):
+            return JournalEntry.objects.filter(
+                published_on=self.published_on,
+                slug=s
+            )
+
+        duplicates = _check_duplicates(slug)
         slug_postfix = 0
 
-        while slug in slugs:
+        while duplicates:
             slug_postfix += 1
             slug_postfix_str  = '-' + str(slug_postfix)
 
@@ -75,12 +70,20 @@ class JournalEntry(models.Model):
                 slug = slug[0:-1]
 
             slug += slug_postfix_str
-            return slug
+            duplicates = check_duplicates(slug)
+
+        return slug
 
     def url(self):
-        return reverse('entry-detail-view', kwargs={
-            'year' : self.published_on.year,
-            'month': self.published_on.month,
-            'day'  : self.published_on.day,
-            'slug' : self.slug
-        })
+        args = {
+            'year': self.published_on.year,
+            'month': self.published_on.strftime('%b'),
+            'day'  : self.published_on.day
+        }
+
+        if self.slug:
+            args['slug'] = self.slug
+            return reverse('entry-detail-view', kwargs=args)
+        else:
+            args['pk'] = self.id
+            return reverse('entry-detail-view-pk', kwargs=args)
