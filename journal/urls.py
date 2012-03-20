06a76ba998a61874b0d8a563e617901430a82513
Ankur Sethi <contact@ankursethi.in>
2012-03-21 00:30:14 +0530

    Reorder URLconf so that primary-key-based entry lookups are tried before slug-based entry lookups. This fixes a bug where Django confused the primary key for the post slug and threw a 404.

--- journal/urls.py
+++ journal/urls.py
@@ -6,12 +6,15 @@ y = '(?P<year>\d{4})'
 m = '(?P<month>\w+)'
 d = '(?P<day>\d{1,2})'
 s = '(?P<slug>\S+)'
+k = '(?P<pk>\d+)'
 
 urlpatterns = patterns('',
     url('^$', ArchiveIndexView.as_view()),
     url('^{0}/$'.format(y), YearArchiveView.as_view()),
     url('^{0}/{1}/$'.format(y, m), MonthArchiveView.as_view()),
     url('^{0}/{1}/{2}/$'.format(y, m, d), DayArchiveView.as_view()),
+    url('^{0}/{1}/{2}/{3}/$'.format(y, m, d, k), EntryView.as_view(),
+        name='entry-detail-view-pk'),
     url('^{0}/{1}/{2}/{3}/$'.format(y, m, d, s), EntryView.as_view(),
-        name='entry-detail-view'),
+        name='entry-detail-view')
 )
