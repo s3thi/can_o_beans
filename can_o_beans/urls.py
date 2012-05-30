b769f9d46c9536cafdd9d4c1af6b4a92c5446167
Ankur Sethi <contact@ankursethi.in>
2012-05-31 00:57:31 +0530

    More auth stuff, some style and template fixes in various places.

--- can_o_beans/urls.py
+++ can_o_beans/urls.py
@@ -9,7 +9,10 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     # url(r'^admin/', include(admin.site.urls)),
 
-    (r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'registration/login.html'}),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'redirect_field_name': '/'},
+        name='auth_login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
+        name='auth_logout'),
     url(r'^journal/', include('journal.urls')),
 )
 
