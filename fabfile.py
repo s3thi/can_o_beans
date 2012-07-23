0f09e4b126c1f943b6c54800183beaeb83d951ca
Ankur Sethi <contact@ankursethi.in>
2012-07-23 10:41:13 +0530

    Added fabfile for deployment.

---
+++ fabfile.py
@@ -0,0 +1,47 @@
+from fabric.api import *
+from fabric.contrib.console import confirm
+import os
+
+REMOTE_PROJECT_DIR = '/home/web/can_o_beans/'
+REMOTE_ACTIVATE_VENV = 'workon cob'
+
+def pull():
+    with cd(REMOTE_PROJECT_DIR):
+        run('git pull')
+
+def compile_stylesheet():
+    remote_static_dir = os.path.join(REMOTE_PROJECT_DIR, 'gravy/static/gravy')
+    with cd(remote_static_dir):
+        run('sass style.scss:style.css')
+
+def collect_static():
+    with cd(REMOTE_PROJECT_DIR):
+        with prefix(REMOTE_ACTIVATE_VENV):
+            run('./manage.py collectstatic')
+
+def migrate():
+    with cd(REMOTE_PROJECT_DIR):
+        with prefix(REMOTE_ACTIVATE_VENV):
+            run('./manage.py migrate')
+
+def stop_uwsgi():
+    run('su -c "supervisorctl stop uwsgi"')
+
+def start_uwsgi():
+    run('su -c "supervisorctl start uwsgi"')
+
+def restart_uwsgi():
+    run('su -c "supervisorctl restart uwsgi"')
+
+def stop_nginx():
+    run('su -c "invoke-rc.d nginx stop"')
+
+def start_nginx():
+    run('su -c "invoke-rc.d nginx start"')
+
+def deploy():
+    pull()
+    compile_stylesheet()
+    collect_static()
+    migrate()
+    restart_uwsgi()
