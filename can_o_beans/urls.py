4d55b945d982437315ff1fd795efdbd3f96b686e
Ankur Sethi <contact@ankursethi.in>
2012-08-28 13:44:24 +0530

    Added a sitemap.

--- can_o_beans/urls.py
+++ can_o_beans/urls.py
@@ -1,19 +1,28 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from journal.models import JournalEntry
 
 from django.contrib import admin
 admin.autodiscover()
 
+
+journal_sitemap_info_dict = {
+    'queryset': JournalEntry.objects.filter(published=True),
+    'date_field': 'published_on'
+}
+
+
+sitemaps = {
+    'flatpages': FlatPageSitemap,
+    'journal': GenericSitemap(journal_sitemap_info_dict)
+}
+
+
 urlpatterns = patterns('',
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
-
-    url(r'^login/$', 'django.contrib.auth.views.login', {'redirect_field_name': '/'},
-        name='auth_login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login',
-        name='auth_logout'),
-
     url(r'^journal/', include('journal.urls')),
+    url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps}),
 )
 
 urlpatterns += staticfiles_urlpatterns()
